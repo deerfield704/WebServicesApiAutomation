95a9a4748c4efebc3fea885507c8620273ec4e42
Emtim <acadiya@mail.ru>
2019-09-12 09:16:19 -0500

    corrected multiply method

--- src/test/java/apiTests/Test.java
+++ src/test/java/apiTests/Test.java
@@ -2,12 +2,14 @@ package apiTests;
 
 public class Test {
     public static void main(String[] args) {
-        int number =10;
-        double number2=20.50;
-        
-        int sum = number+number2;
+        int number = 10;
+        double number2 = 20.50;
+
+
 
     }
-    public static double multiply (double num2, double num3){
-        return num2*num3;
+
+    public static double multiply(double num2, double num3) {
+        return num2 * num3;
+    }
 }
