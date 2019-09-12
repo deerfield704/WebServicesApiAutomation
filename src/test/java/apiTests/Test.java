8e556f18e873d7374f07d3ee9d1812b6a461feb3
deerfield704 <53944821+deerfield704@users.noreply.github.com>
2019-09-11 21:57:09 -0500

    added multiply method.

--- src/test/java/apiTests/Test.java
+++ src/test/java/apiTests/Test.java
@@ -2,6 +2,12 @@ package apiTests;
 
 public class Test {
     public static void main(String[] args) {
+        int number =10;
+        double number2=20.50;
+        
+        int sum = number+number2;
 
     }
+    public static double multiply (double num2, double num3){
+        return num2*num3;
 }
